8ed0e338a5ab1638b12c745ba0bd53e6c46793a7
System Administrator <root@chorta.local>
2011-10-16 18:58:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '84d44bce124a9341e2b8529bc9e6c3056b959aa3d594223e342c56acf0a0086e3f5034eb351806b75e2a0b16dd2d674111fb018c4ddc9e3840a1b802c596fc2a'
